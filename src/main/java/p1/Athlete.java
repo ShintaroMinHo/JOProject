0cba2bf05f4ee12454864c8102eeee9042a21e01
ShuntianZHANG <shzh61655@eleve.isep.fr>
2024-05-17 17:02:02 +0200

    athlete

---
+++ src/main/java/p1/Athlete.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class Athlete{
+    private int id;
+    private String name;
+    private String nationality;
+    private int age;
+    private String gender;
+    public Athlete(int id,String name ,String nationality,int age ,String gender){
+        this.id = id;
+        this.name = name;
+        this.nationality = nationality;
+        this.age=age;
+        this.gender=gender;
+    }
+}
