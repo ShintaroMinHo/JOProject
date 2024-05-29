975439da8201e955f37fe40ae66edf97cba64a0e
PSTRengar <80516082+PSTRengar@users.noreply.github.com>
2024-05-29 11:14:39 +0200

    Event UI change

--- src/main/java/UI/EventUI.java
+++ src/main/java/UI/EventUI.java
@@ -1,4 +1,18 @@
 package UI;
 
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+
 public class EventUI {
+    private VBox view;
+
+    public EventUI() {
+        view = new VBox();
+        view.getChildren().add(new Label("Event Management Section"));
+    }
+
+    public VBox getView() {
+        return view;
+    }
 }
+
